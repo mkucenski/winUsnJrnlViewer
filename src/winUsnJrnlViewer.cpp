6e612c40561b3013b27f3479fe42d895c2f6b9cb
Matt Kucenski <matthew.kucenski@doe.gov>
2017-03-24 10:47:10 -0600

    initial, basic, somewhat tested and useful form

--- src/winUsnJrnlViewer.cpp
+++ src/winUsnJrnlViewer.cpp
@@ -12,9 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//#define _DEBUG_ 1
+// #define _DEBUG_ 1
 
-#include "libWinUsnJrnl/src/winUsnJrnlRecordsFile.h"
+#include "libWinUsnJrnl/src/winUSNJournal.h"
+#include "libWinUsnJrnl/src/winUSNRecord.h"
 #include "libtimeUtils/src/timeZoneCalculator.h"
 #include "libtimeUtils/src/timeUtils.h"
 
@@ -26,31 +27,43 @@ using namespace std;
 
 #include "misc/stringType.h"
 #include "misc/poptUtils.h"
+#include "misc/tsk_mactime.h"
+#include "misc/boost_lexical_cast_wrapper.hpp"
 
 #define OPT_DELIMITED		10
 #define OPT_START_DATE		20
 #define OPT_END_DATE			30
 #define OPT_WITH_FILENAME	40
+#define OPT_NO_FILENAME		45
 #define OPT_MACTIME			50
 #define OPT_TIMEZONE			60
 #define OPT_VERSION			200
 
+//Sleuthkit TSK3.x body format
+//0  |1        |2    |3     |4       |5       |6   |7    |8    |9    |10
+//MD5|NAME     |INODE|PERMS |UID     |GID     |SIZE|ATIME|MTIME|CTIME|CRTIME
+//
 int main(int argc, const char** argv) {
 	int rv = EXIT_FAILURE;
 	
+	vector<string> vectorFilenames;
 	bool bDelimited = false;
 	string strDateStart;
 	string strDateEnd;
 	bool bWithFilename = false;
+	bool bNoFilename = false;
 	bool bMactime = false;
 	
 	timeZoneCalculator tzcalc;
 
+	//TODO	Another interesting upgrade would be options to find/filter based on filename and/or inode (MFT+Seq). 
+	//			This would allow visualizing a "history" of a certain file.
 	struct poptOption optionsTable[] = {
 		{"delimited", 			'd',	POPT_ARG_NONE,		NULL,	OPT_DELIMITED,			"Display in comma-delimited format.", NULL},
 		{"start-date", 		0,		POPT_ARG_STRING,	NULL,	OPT_START_DATE, 		"Only display entries recorded after the specified date.", "mm/dd/yyyy"},
 		{"end-date", 		 	0,		POPT_ARG_STRING,	NULL,	OPT_END_DATE, 			"Only display entries recorded before the specified date.", "mm/dd/yyyy"},
 		{"with-filename",		'H',	POPT_ARG_NONE,		NULL,	OPT_WITH_FILENAME,	"Display filename in output. Useful when batch processing multiple files.", NULL},
+		{"no-filename", 		'h',	POPT_ARG_NONE,		NULL,	OPT_NO_FILENAME,	 	"Suppress filename in output.", NULL},
 		{"mactime",				'm', 	POPT_ARG_NONE, 	NULL,	OPT_MACTIME, 			"Display in the SleuthKit's mactime format."},
 		{"timezone", 			'z',	POPT_ARG_STRING,	NULL,	OPT_TIMEZONE,			"POSIX timezone string (e.g. 'EST-5EDT,M4.1.0,M10.1.0' or 'GMT-5') to be used when displaying data. Defaults to GMT.", "zone"},
 		{"version",	 			0,		POPT_ARG_NONE,		NULL,	OPT_VERSION,			"Display version.", NULL},
@@ -89,6 +102,9 @@ int main(int argc, const char** argv) {
 			case OPT_WITH_FILENAME:
 				bWithFilename = true;
 				break;
+			case OPT_NO_FILENAME:
+				bNoFilename = true;
+				break;
 			case OPT_MACTIME:
 				bMactime = true;
 				break;
@@ -114,185 +130,92 @@ int main(int argc, const char** argv) {
 	
 	const char* cstrFilename = poptGetArg(optCon);
 	while (cstrFilename) {
-		arguments.filenameVector.push_back(cstrFilename);
+		vectorFilenames.push_back(cstrFilename);
 		cstrFilename = poptGetArg(optCon);
 	}
 	
-	if (arguments.filenameVector.size() < 1) {
-		usage(optCon, "You must specify at least one file", "e.g., $Extend\$UsnJrnl:$J");
+	if (vectorFilenames.size() < 1) {
+		usage(optCon, "You must specify at least one file", "e.g., $Extend\\$UsnJrnl:$J");
 		exit(EXIT_FAILURE);
 	}
 	
-	if (arguments.bDelimited) {
+	if (bDelimited) {
 		cout << "Time Zone: \"" << tzcalc.getTimeZoneString() << "\"" << endl;		//Display the timezone so that the reader knows which zone was used for this output
-		if ((arguments.filenameVector.size() > 1 && arguments.bNoFilename == false) || arguments.bWithFilename == true) {
-			printf("File,");
+		if ((vectorFilenames.size() > 1 && bNoFilename == false) || bWithFilename == true) {
+			cout << "File,";
 		}
-		printf("Record,Offset,Type,Date,Time,Source,Category,Event,SID,Computer\n");
+		cout << "Inode,Parent Inode,USN,Date,Time,Reasons,Sources,Security ID,File Attributes,Filename\n";
 	}
 	
-	long lRecordCount = 0;
-	for (vector<string>::iterator it = arguments.filenameVector.begin(); it != arguments.filenameVector.end(); it++) {
-		winEventFile eventFile(*it);
+	long cRecords = 0;
+	for (vector<string>::iterator it = vectorFilenames.begin(); it != vectorFilenames.end(); it++) {
+		winUSNJournal CjournalFile(*it);
 
-		if (arguments.bMactime) {
-			winEvent* pEvent = NULL;
-			while (eventFile.getNextRecord(&pEvent) == WIN_EVENT_SUCCESS) {
-				if (displayEvent(pEvent, &arguments) == true) {
-					lRecordCount++;
-					
-					string strTimeWritten = getTimeString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeWritten())));
-					string strDateWritten = getDateString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeWritten())));
-					string strTimeGenerated = getTimeString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeGenerated())));
-					string strDateGenerated = getDateString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeGenerated())));
-			
-					printf("USN|%s|%lu|%lu||%ls||%s|%ls||%s||%lu|%lu||",
-							it->c_str(),
-							pEvent->getRecordNumber(),
-							pEvent->getEventCode(),
-							pEvent->getSourceName().c_str(),
-							//pEvent->getSIDString().c_str(),
-							(matchSIDtoUsernames ? pwdFile.getUsernameBySID(pEvent->getSIDString()).c_str() : pEvent->getSIDString().c_str()),
-							pEvent->getComputerName().c_str(),
-							getEventTypeString(pEvent->getEventType()).c_str(),
-							pEvent->getTimeWritten(),
-							pEvent->getTimeGenerated()
-					);
-					printf("\n");
-				}	//if (displayEvent(pEvent, &arguments) == true) {
-				delete pEvent;
-				pEvent = NULL;
-			}	//while (eventFile.getNextRecord(&pEvent) == WIN_EVENT_SUCCESS) {
-		} else {	
-			if (arguments.bDelimited) {
-				winEvent* pEvent = NULL;
-				while (eventFile.getNextRecord(&pEvent) == WIN_EVENT_SUCCESS) {
-					if (displayEvent(pEvent, &arguments) == true) {
-						lRecordCount++;
+		winUSNRecord* p_clsUSNRecord = NULL;
+		while (CjournalFile.getNextRecord(&p_clsUSNRecord) == USNJRNL_SUCCESS) {
+			cRecords++;
 
-						string strTimeWritten = getTimeString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeWritten())));
-						string strDateWritten = getDateString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeWritten())));
-						string strTimeGenerated = getTimeString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeGenerated())));
-						string strDateGenerated = getDateString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeGenerated())));
+			DEBUG_INFO("winUsnJrnlViewer p_clsUSNRecord.dtmTimestamp=" << p_clsUSNRecord->getTimestamp());
+			string strTime = getTimeString(tzcalc.calculateLocalTime(getFromWindows64(p_clsUSNRecord->getTimestamp())));
+			string strDate = getDateString(tzcalc.calculateLocalTime(getFromWindows64(p_clsUSNRecord->getTimestamp())));
 
-						if ((arguments.filenameVector.size() > 1 && arguments.bNoFilename == false) || arguments.bWithFilename == true) {
-							printf("%s,", it->c_str());
-						}
-						printf("%lu,0x%lx,%s,%s,%s,%ls,%d,%lu,%s,%ls",
-								pEvent->getRecordNumber(),
-								pEvent->getRecordOffset(),
-								getEventTypeString(pEvent->getEventType()).c_str(),
-								strDateGenerated.c_str(),//pEvent->getDateGeneratedString().c_str(),
-								strTimeGenerated.c_str(),//pEvent->getTimeGeneratedString().c_str(),
-								pEvent->getSourceName().c_str(),
-								pEvent->getEventCategory(),
-								pEvent->getEventCode(),
-								//pEvent->getSIDString().c_str(),
-								(matchSIDtoUsernames ? pwdFile.getUsernameBySID(pEvent->getSIDString()).c_str() : pEvent->getSIDString().c_str()),
-								pEvent->getComputerName().c_str()
-						);
-						if (arguments.bStringColumns == true) {
-							vector<string_t> vStrings;
-							if (pEvent->getStrings(&vStrings) == WIN_EVENT_SUCCESS) {
-								for (vector<string_t>::iterator it = vStrings.begin(); it != vStrings.end(); it++) {
-									printf(",\"%ls\"", removeNewLines(&(*it), STR(" _ ")).c_str());
-								}
-							}	//if (pEvent->getStrings(&vStrings) == WIN_EVENT_SUCCESS) {
-						}	//if (arguments.bStringColumns == true) {
-						printf("\n");
-					}	//if (displayEvent(pEvent->getEventCode(), &arguments) == true) {
-					
-					delete pEvent;
-					pEvent = NULL;
-				}	//while (eventFile.getNextRecord(&pEvent) == WIN_EVENT_SUCCESS) {
-			} else {	//if (bDelimited) {
-				winEvent* pEvent = NULL;
-				while (eventFile.getNextRecord(&pEvent) == WIN_EVENT_SUCCESS) {
-					if (displayEvent(pEvent, &arguments) == true) {
-						lRecordCount++;
+			if (bMactime) {
+				// TODO 	An interesting upgrade to this project would be to modify whether the timestamp is associated m,a,c,or b
+				// 		based on the action noted in the change journal (USN_REASON).
+				cout 	<< "|"
+						<< p_clsUSNRecord->getFilename() << " (" << p_clsUSNRecord->getReasons() << ")|"
+						<< p_clsUSNRecord->getMFTEntry() << " (" << p_clsUSNRecord->getMFTSeq() << ")|"
+						<< "winusnjrnl--" << "|"
+						<< "|"
+						<< "|"
+						<< "|"
+						<< "|"
+						<< getUnix32FromWindows64(p_clsUSNRecord->getTimestamp()) << "|"
+						<< "|"
+						<< "\n";
+			} else {
+				if (bDelimited) {
+					// File,Inode,Parent Inode,USN,Time,Reasons,Sources,Security ID,File Attributes,Filename
+					if ((vectorFilenames.size() > 1 && bNoFilename == false) || bWithFilename == true) {
+						cout << it->c_str() << ",";
+					}
+					cout 	<< p_clsUSNRecord->getMFTEntry() << ","
+							<< p_clsUSNRecord->getParentMFTEntry() << ","
+							<< p_clsUSNRecord->getUSN() << ","
+							<< strDate << ","
+							<< strTime << ","
+							<< "\"" << p_clsUSNRecord->getReasons() << "\","
+							<< "\"" << p_clsUSNRecord->getSources() << "\","
+							<< p_clsUSNRecord->getSecurityID() << ","
+							<< "\"" << p_clsUSNRecord->getFileAttrs() << "\","
+							<< "\"" << p_clsUSNRecord->getFilename() << "\"\n";
+				} else {
+					if ((vectorFilenames.size() > 1 && bNoFilename == false) || bWithFilename == true) {
+						cout << it->c_str() << " ";
+					}
+					cout	<< "USN " 						<< p_clsUSNRecord->getUSN() << " "
+							<< "(offset=" 					<< p_clsUSNRecord->getRecordPos() 		<< ", length=" 	<< p_clsUSNRecord->getRecordLen() << "):\n"
+							<< "\tFilename:\t\t"			<< p_clsUSNRecord->getFilename() 		<< "\n"
+							<< "\tDate:\t\t\t"			<< strDate										<< "\tTime:\t"		<< strTime << " (" << tzcalc.getTimeZoneString()	<< ")\n"
+							<< "\tMFT Entry:\t\t"		<< p_clsUSNRecord->getMFTEntry() 		<< "\tSeq:\t\t" 	<< p_clsUSNRecord->getMFTSeq() << "\n"
+							<< "\tParent MFT Entry:\t"	<< p_clsUSNRecord->getParentMFTEntry()	<< "\tSeq:\t\t"	<< p_clsUSNRecord->getParentMFTSeq() << "\n"
+							<< "\tUSN Version:\t\t"		<< p_clsUSNRecord->getVersion() 			<< "\n"
+							<< "\tSecurity ID:\t\t"		<< p_clsUSNRecord->getSecurityID()		<< "\n"
+							<< "\tReasons:\t\t"			<< p_clsUSNRecord->getReasons()			<< "\n"
+							<< "\tSources:\t"				<< p_clsUSNRecord->getSources()			<< "\n"
+							<< "\tFile Attributes:\t"	<< p_clsUSNRecord->getFileAttrs()		<< "\n"
+							<< "----------------------------------------------------------------------------------------------------\n";
+				}
+			}
 
-						string strTimeWritten = getTimeString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeWritten())));
-						string strDateWritten = getDateString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeWritten())));
-						string strTimeGenerated = getTimeString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeGenerated())));
-						string strDateGenerated = getDateString(tzcalc.calculateLocalTime(getFromUnix32(pEvent->getTimeGenerated())));
+			delete p_clsUSNRecord;
+			p_clsUSNRecord = NULL;
 
-						if ((arguments.filenameVector.size() > 1 && arguments.bNoFilename == false) || arguments.bWithFilename == true) {
-							printf("%s ", it->c_str());
-						}
-						printf("%lu: (offset=0x%lx, length=0x%lx)\n\tDate Generated:\t\t%s\t\tSource:\t\t%ls\n\tTime Generated (GMT):\t%s\t\tCategory:\t%u\n\tDate Written:\t\t%s\t\tEvent ID:\t%lu\n\tTime Written (GMT):\t%s\t\tUser:\t\t%s\n\tType:\t\t\t%-10s\t\tComputer:\t%ls\n",
-								pEvent->getRecordNumber(),
-								pEvent->getRecordOffset(),
-								pEvent->getRecordLength(),
-								strDateGenerated.c_str(),//pEvent->getDateGeneratedString().c_str(),
-								pEvent->getSourceName().c_str(),
-								strTimeGenerated.c_str(),//pEvent->getTimeGeneratedString().c_str(),
-								pEvent->getEventCategory(),
-								strDateWritten.c_str(),//pEvent->getDateWrittenString().c_str(),
-								pEvent->getEventCode(),
-								strTimeWritten.c_str(),//pEvent->getTimeWrittenString().c_str(),
-								//pEvent->getSIDString().c_str(),
-								(matchSIDtoUsernames ? pwdFile.getUsernameBySID(pEvent->getSIDString()).c_str() : pEvent->getSIDString().c_str()),
-								getEventTypeString(pEvent->getEventType()).c_str(),
-								pEvent->getComputerName().c_str()
-						);
-			
-						vector<string_t> vStrings;
-						if (pEvent->getStrings(&vStrings) == WIN_EVENT_SUCCESS) {
-							printf("\n\tStrings: (offset=0x%lx, count=%u)\n", pEvent->getStringsOffset(), pEvent->getNumStrings());
-					
-							int i=1;
-							for (vector<string_t>::iterator it = vStrings.begin(); it != vStrings.end(); it++) {
-								printf("\t%7d:\t%ls\n", i++, removeNewLines(&(*it), STR(" _ ")).c_str());
-							}
-						}	//if (pEvent->getStrings(&vStrings) == WIN_EVENT_SUCCESS) {
-						
-						if (pEvent->getDataLength() > 0) {
-							unsigned char* pData = NULL;
-							if (pEvent->getData((char**)&pData) == WIN_EVENT_SUCCESS) {
-								unsigned long ulDataLength = pEvent->getDataLength();
-								printf("\n\tData: (offset=0x%lx, length=0x%lx)\n", pEvent->getDataOffset(), ulDataLength);
-						
-								unsigned long i,j;
-								for (i=0; i<ulDataLength; ) {
-									printf("\t   %04ld:\t", i);
-									for (j=i; j<ulDataLength && j<i+8; j++) {
-										printf("%s%02x", (j==i ? "" : " "), pData[j]);
-									}
-									for (; j<i+8; j++) {
-										printf("   ");
-									}
-									printf("\t\t");
-									for (j=i; j<ulDataLength && j<i+8; j++) {
-										if (pData[j] == '\0') {
-											printf(" .");
-										} else if (pData[j] < 0x20 || pData[j] > 0x7e) {
-											printf(" _");
-										} else {
-											printf(" %c", pData[j]);
-										}
-									}	//for (j=i; j<ulDataLength && j<i+8; j++) {
-									for (; j<i+8; j++) {
-										printf("  ");
-									}
-									printf("\n");
-									i = j;
-								}	//for (i=0; i<ulDataLength; ) {
-								
-								free(pData);
-								pData = NULL;
-							}	//if (pEvent->getData((char**)&pData) == WIN_EVENT_SUCCESS) {
-						}	//if (pEvent->getDataLength() > 0) {
-						
-						printf("----------------------------------------------------------------------------------------------------\n");
-					}	//if (displayEvent(pEvent->getEventCode(), &eventsVector) == true) {
-					
-					delete pEvent;
-					pEvent = NULL;
-				}	//while (eventFile.getNextRecord(&pEvent) == WIN_EVENT_SUCCESS) {
-			}	//else {	//if (bDelimited) {
-		}	//if (arguments.bMactime) {
-	}	//for (vector<string>::iterator it = arguments.filenameVector.begin(); it != arguments.filenameVector.end(); it++) {
-	printf("\nRecord Count: %ld\n", lRecordCount);
-		
+		} //while (CjournalFile.getNextRecord(&p_clsUSNRecord) == USNJRNL_SUCCESS)
+
+	} //for (vector<string>::iterator it = vectorFilenames.begin(); it != vectorFilenames.end(); it++)
+
+	cout << "Record Count: " << cRecords << "\n";
+
 	exit(rv);	
-}	//int main(int argc, const char** argv) {
+}	//int main(int argc, const char** argv)
